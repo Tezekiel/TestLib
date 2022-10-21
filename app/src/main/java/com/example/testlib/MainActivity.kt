cd7ae12e5e1dda13596d427b862e9d5488fe3e4f
kkarlo <kkaragic@gmail.com>
2022-10-21 12:42:03 +0200

    Remove maven plugin 2

--- app/src/main/java/com/example/testlib/MainActivity.kt
+++ app/src/main/java/com/example/testlib/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.testlib
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.library.reverseMe
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        "karlo".reverseMe()
     }
 }
