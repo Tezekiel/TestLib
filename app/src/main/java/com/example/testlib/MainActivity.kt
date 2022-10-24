7598618a6832e900acfab7bb4f3d5296ca7a64c6
kkarlo <kkaragic@gmail.com>
2022-10-24 09:18:28 +0200

    Try again 1.5

--- app/src/main/java/com/example/testlib/MainActivity.kt
+++ app/src/main/java/com/example/testlib/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.testlib
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.library.reverseMe
+import com.example.mytestlibrary.reverseUs
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         "karlo".reverseMe()
+        "karlo".reverseUs()
     }
 }
---
+++ mytestlibrary/src/main/java/com/example/mytestlibrary/extensions.kt
@@ -0,0 +1,3 @@
+package com.example.mytestlibrary
+
+fun String.reverseUs() = this.reversed()
